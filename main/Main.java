0075cfafbe15357b92a0b73c0cd7a16fbd123b9c
Dima Zagorodny <dima@ivtech.co.il>
2019-03-08 21:43:29 +0200

    Init

---
+++ main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Main {
+
+    public Main() {
+
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.showMessage();
+    }
+
+    private void showMessage() {
+        System.out.println("Hello World!");
+    }
+
+}
